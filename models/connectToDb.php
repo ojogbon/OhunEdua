cbbbffc83ebde13855bc27818ef136f3918d22e1
ojogbon <40061054+ojogbon@users.noreply.github.com>
2022-06-22 23:22:10 +0100

    fixing db

--- models/connectToDb.php
+++ models/connectToDb.php
@@ -7,13 +7,13 @@
 		private $host ;
 		private $password;
 		private $database;
-
+		
 		protected function doConnect(){
 			$this->user = "root";
 			$this->password = "";
 			$this->host = "localhost";
 			$this->database ="ohun";
-
+			echo "343434" ;
 		$conn = new mysqli($this->host,$this->user,$this->password,$this->database);
 		return $conn;
 		}
