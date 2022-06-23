8aa57fe0b0b0cfb7b7478fb9097bb3ab38abc1af
ojogbon <40061054+ojogbon@users.noreply.github.com>
2022-06-23 08:02:16 +0100

    fixing db stagging

--- models/connectToDb.php
+++ models/connectToDb.php
@@ -9,11 +9,13 @@
 		private $database;
 		
 		protected function doConnect(){
-			$this->user = "root";
-			$this->password = "";
-			$this->host = "localhost";
-			$this->database ="ohun";
-			echo "343434" ;
+			$dev_type__ = 'stage'; 
+
+			$this->user =  ($dev_type__ === "dev") ? "root" : "ba382c9a507553";
+			$this->password = $dev_type__ === "dev" ? "" : "b50f18d0";
+			$this->host = ($dev_type__ === "dev") ? "localhost" : "us-cdbr-east-02.cleardb.com";
+			$this->database = ($dev_type__ === "dev") ? "ohun" : "heroku_b45b9c616f211d6";
+		
 		$conn = new mysqli($this->host,$this->user,$this->password,$this->database);
 		return $conn;
 		}
